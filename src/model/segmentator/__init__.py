1ebcab1f0e32268dc095d83676a9e7cd44c1d1a6
Min-Sheng <mason840929@gmail.com>
2019-11-21 17:09:03 +0800

    make segmentator

---
+++ src/model/segmentator/__init__.py
@@ -0,0 +1,1 @@
+from .segmentator import Segmentator
---
+++ src/model/segmentator/general.py
@@ -0,0 +1,5 @@
+import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+from torch.optim import lr_scheduler
